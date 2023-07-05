c4fd85fa010a80b13785ec1578a236ef3fbc70bb
Nandhan6875 <119912234+blueloc@users.noreply.github.com>
2023-07-05 14:17:01 +0530

    Update part4Whileloop.java

--- java_programs/untitled/src/test7Part1/part4Whileloop.java
+++ java_programs/untitled/src/test7Part1/part4Whileloop.java
@@ -255,14 +255,269 @@ import java.util.Scanner;
 }   */
 
 
-   public class part4Whileloop  {
+ /*  public class part4Whileloop  {
        public static void main(String[] args) {
 
           // 69.Write a program to find the prime numbers between 1 and 100 using a while loop.
 
           int i=2;
-          while (i<=100) {
-              
-          }
+           System.out.println("The prime numbers between 1 and 100 are ");
+
+           while (i<=100) {
+               boolean isprime=true;
+
+               for (int j=2;j*j<=i;j++) {
+                   if (i%j==0) {
+                       isprime = false;
+                       break;
+                   }
+               }
+               if (isprime) {
+                   System.out.println(i);
+               }
+               i++;
+
+           }
+
+
+
        }
+}      */
+
+
+
+ /* public class part4Whileloop  {
+      public static void main(String[] args) {
+
+        // 70.Write a program to check if a given number is a palindrome using a while loop.
+
+
+
+      }
+}       */
+
+
+
+ /*  public class part4Whileloop  {
+       public static void main(String[] args) {
+
+          //71.Write a Java program to multiply two numbers.
+
+           int a =4;
+           int b=5;
+           int c= a*b;
+           System.out.println("the sum of two numbers " + c  );
+       }
+}    */
+
+
+/*    public class part4Whileloop  {
+        public static void main(String[] args) {
+
+          //71.Write a Java program to multiply two numbers.
+
+          Scanner scan= new Scanner(System.in);
+            System.out.println("Enter the Firstnumber");
+            int firstNumber = scan.nextInt();
+
+            System.out.println("Enter the SecondNumber");
+            int secondNumber = scan.nextInt();
+
+            int product = firstNumber * secondNumber;
+
+            System.out.println(firstNumber + "*" + secondNumber + "=" + product);
+        }
+}    */
+
+
+
+ /* public class part4Whileloop  {
+     public static void main(String[] args) {
+
+
+       // 72.Write a Java program to divide two numbers.
+
+         Scanner scan = new Scanner(System.in);
+
+         System.out.println(" Enter the first number");
+         int firstNumber = scan.nextInt();
+
+         System.out.println("Enter the second number");
+         int secondNumber = scan.nextInt();
+
+         int divide = firstNumber / secondNumber;
+
+         System.out.println(firstNumber + "/" + secondNumber + " is "  + divide);
+     }
+}    */
+
+
+
+ /*  public class part4Whileloop {                         // for remainder (%)
+       public static void main(String[] args) {
+
+           // 73.Write a Java program to find the remainder when dividing two numbers.
+
+
+           Scanner scan = new Scanner(System.in);
+
+           System.out.println("Enter the firstnumber");
+           int firstNumber = scan.nextInt();
+
+           System.out.println("Enter the secondNumber");
+           int secondNumber = scan.nextInt();
+
+           int remainder = firstNumber % secondNumber;
+
+           System.out.println(firstNumber + "%" + secondNumber + " remainder is " + remainder) ;
+       }
+}      */
+
+
+/*  public class part4Whileloop  {
+      public static void main(String[] args) {
+
+        // 74.Write a Java program to calculate the average of three numbers.
+
+          Scanner scan= new Scanner(System.in);
+
+          System.out.println("Enter the firstnumber");
+          int firstNumber = scan.nextInt();
+
+          System.out.println("Enter the Secondnumber");
+          int secondNumber = scan.nextInt();
+
+          System.out.println("Enter the Thirdnumber");
+          int thirdNumber = scan.nextInt();
+
+          int average = (firstNumber+secondNumber+thirdNumber) / 2;
+
+          System.out.println("the Average of three numbers is " + average);
+      }
+}          */
+
+
+/*  public  class part4Whileloop  {
+      public static void main(String[] args) {
+
+         // 75.Write a Java program to find the maximum of two numbers.
+
+         int a=12;
+         int b=5;
+        // int max = max(a,b);
+          System.out.println("the max is " + Math.max(a,b));
+      }
+}   */
+
+
+ /*  public class part4Whileloop  {
+      public static void main(String[] args) {
+
+         // 75.Write a Java program to find the maximum of two numbers.
+
+
+         Scanner scan= new Scanner(System.in);
+          System.out.println("Enter the firstNumber");
+          int firstNumber = scan.nextInt();
+
+          System.out.println("Enter the secondNumber");
+          int secondNumber = scan.nextInt();
+
+          System.out.println("the Maximum of two numbers is " + Math.max(firstNumber,secondNumber));
+      }
+}     */
+
+
+
+ /*  public class part4Whileloop  {
+       public static void main(String[] args) {
+
+          // 76.Write a Java program to find the minimum of two numbers.
+
+           Scanner scan = new Scanner(System.in);
+
+           System.out.println("Enter the firstnumber: ");
+           double firstNumber = scan.nextDouble();
+
+           System.out.println("Enter the second number: ");
+           double secondNumber = scan.nextDouble();
+
+           System.out.println("The minimum of two numbers is " + Math.min(firstNumber,secondNumber));
+       }
+}     */
+
+
+
+/*  public class part4Whileloop  {
+     public static void main(String[] args) {
+
+        // 77.Write a Java program to calculate the square of a number.
+
+        Scanner scan =new Scanner(System.in);
+         System.out.println("Enter the number");
+         double number = scan.nextDouble();
+
+         System.out.println("The square of the given number is " + number * number);
+     }
+}       */
+
+
+ /*  public class part4Whileloop  {
+       public static void main(String[] args) {
+
+          // 78.Write a Java program to calculate the cube of a number.
+
+           Scanner scan= new Scanner(System.in);
+           System.out.println("Enter the  number: ");
+           double number = scan.nextDouble();
+
+           System.out.println("The cube of given number is " + number * number * number);
+       }
+}            */
+
+
+
+
+ /*  public class part4Whileloop  {
+       public static void main(String[] args) {
+
+          // 78.Write a Java program to calculate the cube of a number.
+
+           Scanner scan= new Scanner(System.in);
+           System.out.println("Enter the  number: ");
+           double number = scan.nextDouble();
+
+           System.out.println("The cube of given number is " +  Math.sqrt(number));
+       }
+}   */
+
+
+
+ /*  public class part4Whileloop  {
+       public static void main(String[] args) {
+
+          // 79.Write a Java program to calculate the square root of a number.
+
+           Scanner scan = new Scanner(System.in);
+           System.out.println("Enter the number: ");
+           double number = scan.nextDouble();
+
+           System.out.println("The square root of given number is " + Math.sqrt(number));    //  Math.sqrt()
+       }
+}     */
+
+
+  public class part4Whileloop  {
+      public static void main(String[] args) {
+
+         // 80.Write a Java program to calculate the factorial of a number.
+
+          Scanner scan = new Scanner(System.in);
+          System.out.println("Enter the number");
+          int number = scan.nextInt();
+
+          int fact= number
+
+
+      }
 }
